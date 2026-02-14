361c9f180ded9664e020542e52f62516a9eb7de8
Ait Baha Said <said.ait-baha1@etu.univ-lorraine.fr>
2026-02-14 08:58:34 +0100

    configuration node js ainsi que ajout d'application todo list

---
+++ todo.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TodoApp = /** @class */ (function () {
+    function TodoApp() {
+        this.tasks = [];
+        this.id = 1;
+    }
+    TodoApp.prototype.add = function (title) {
+        this.tasks.push({
+            id: this.id++,
+            title: title,
+            completed: false
+        });
+    };
+    TodoApp.prototype.list = function () {
+        console.log('Taches:');
+        this.tasks.forEach(function (t) {
+            console.log("".concat(t.id, ". [").concat(t.completed ? 'x' : ' ', "] ").concat(t.title));
+        });
+    };
+    TodoApp.prototype.done = function (id) {
+        var task = this.tasks.find(function (t) { return t.id === id; });
+        if (task) {
+            task.completed = true;
+        }
+        else {
+            console.log("Tache avec id ".concat(id, " non trouv\u00E9e."));
+        }
+    };
+    return TodoApp;
+}());
+exports.default = TodoApp;
+var monApp = new TodoApp();
+monApp.add('Apprendre TypeScript');
+monApp.add('Faire les courses');
+console.log('----début-------:');
+monApp.list();
+console.log('----marquer la tache 1 comme faite-------:');
+monApp.done(1);
+monApp.list();
